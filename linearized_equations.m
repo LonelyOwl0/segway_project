612da8151284790452c8f4594480a1961fc57c58
Fouad Mohammed <65251758+LonelyOwl0@users.noreply.github.com>
2023-12-11 10:29:44 +0100

    Update linearized_equations.m

--- linearized_equations.m
+++ linearized_equations.m
@@ -1,35 +1,36 @@
-function dx = nonlinear_Segway_PT(input_vector)
-    % Extract inputs from input_vector
-    x = input_vector(1); % Position
-    theta_2 = input_vector(2); % Position
-    v = input_vector(3); % Velocity
-    omega_2 = input_vector(4); % Velocity
-    tau = input_vector(5); % Torque
-    m_1 = input_vector(6); % Mass
-    m_2 = input_vector(7); % Mass
-    I_1 = input_vector(8); % Inertia
-    I_2 = input_vector(9); % Inertia
-    L = input_vector(10); % Length
-    r = input_vector(11); % Radius
+function dx_sortie = dynamique_Segway_NonLineaire(vecteur_entree)
+    % Extraire les entrées de vecteur_entree
+    position_x = vecteur_entree(1); % Position
+    angle_theta_2 = vecteur_entree(2); % Position
+    vitesse_v = vecteur_entree(3); % Vitesse
+    vitesse_angulaire_omega_2 = vecteur_entree(4); % Vitesse
+    couple_tau = vecteur_entree(5); % Couple
+    masse_m_1 = vecteur_entree(6); % Masse
+    masse_m_2 = vecteur_entree(7); % Masse
+    inertie_I_1 = vecteur_entree(8); % Inertie
+    inertie_I_2 = vecteur_entree(9); % Inertie
+    longueur_L = vecteur_entree(10); % Longueur
+    rayon_r = vecteur_entree(11); % Rayon
 
-    % Define quantities
-    g = 9.81;
-    d_11 = (m_1 + m_2) * r^2 + I_1;
-    d_12 = m_2 * r * L*cos(theta_2);
-    d_22 = m_2 * L^2 + I_2;
+    % Définir les quantités
+    gravite_g = 9.81;
+    d_11 = (masse_m_1 + masse_m_2) * rayon_r^2 + inertie_I_1;
+    d_12 = masse_m_2 * rayon_r * longueur_L * cos(angle_theta_2);
+    d_22 = masse_m_2 * longueur_L^2 + inertie_I_2;
     Delta = d_11 * d_22 - d_12^2;
 
-    % Implement Eq. (15) with small angles approximation
-    dx = v;
-    dtheta_2 = omega_2;
+    % Implémenter l'équation (15) avec l'approximation des petits angles
+    dx_vitesse = vitesse_v;
+    dtheta_2 = vitesse_angulaire_omega_2;
 
-    % Use small angles approximation for gravitational and centrifugal terms
-    h_1 = -m_2 * r * L * theta_2 * omega_2^2;
-    h_2 = -m_2 * g * L * theta_2;
+    % Utiliser l'approximation des petits angles pour les termes gravitationnels et centrifuges
+    h_1 = -masse_m_2 * rayon_r * longueur_L * angle_theta_2 * vitesse_angulaire_omega_2^2;
+    h_2 = -masse_m_2 * gravite_g * longueur_L * angle_theta_2;
 
-    dv = (d_22 * (tau - h_1) + d_12 * (0+ h_2)) / r*Delta;
-    domega_2 = (-d_12 * (tau - h_1) - d_11 * (0 + h_2)) / Delta;
+    dvitesse_v = (d_22 * (couple_tau - h_1) + d_12 * (0 + h_2)) / rayon_r * Delta;
+    dvitesse_angulaire_omega_2 = (-d_12 * (couple_tau - h_1) - d_11 * (0 + h_2)) / Delta;
 
-    % Output dx
-    dx = [dx; dtheta_2; dv; domega_2];
+    % Sortie dx_sortie
+    dx_sortie = [dx_vitesse; dtheta_2; dvitesse_v; dvitesse_angulaire_omega_2];
 end
+
